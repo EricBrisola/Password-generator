7a4a2aaf3dc74e2adf5895686c92edbcc1a14e72
unknown <eric.coca13@gmail.com>
2023-12-08 15:01:43 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import PsdGenerator from "./components/PsdComponent/PsdGenerator";
+
+function App() {
+  return (
+    <div className="App">
+      <PsdGenerator />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PsdComponent/PsdGenerator.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { useState } from "react";
+import "./psdGenerator.css";
+
+function PsdGenerator() {
+  const [currentPsd, setCurrentPsd] = useState("");
+  const [upperCaseCb, setUpperCaseCb] = useState(false);
+  const [lowerCaseCb, setLowerCasecb] = useState(false);
+  const [numbersCb, setNumbersCb] = useState(false);
+  const [specialsCb, setSpecialsCb] = useState(false);
+
+  const changeCbValueUpper = () => setUpperCaseCb(!upperCaseCb);
+  const changeCbValueLower = () => setLowerCasecb(!lowerCaseCb);
+  const changeCbValueNums = () => setNumbersCb(!numbersCb);
+  const changeCbValueSpec = () => setSpecialsCb(!specialsCb);
+
+  const generatePsd = (upperCaseCb, lowerCaseCb, numbersCb, specialsCb) => {
+    const allOptions = [
+      { chosen: upperCaseCb, value: "ABCDEFGHIJKLMNOPQRSTUVWXYZ" },
+      { chosen: lowerCaseCb, value: "abcdefghijklmnopqrstuvwxyz" },
+      { chosen: numbersCb, value: "0123456789" },
+      { chosen: specialsCb, value: "!@#$%&*()_+-=[]{}|:'<>,./?" },
+    ];
+
+    const chosenOptions = allOptions.filter((option) => option.chosen === true);
+    const valuesForPsd = chosenOptions.map((e) => e.value).join("");
+    //console.log(valuesForPsd);
+
+    const generatedPsd = [];
+    for (let i = 0; i < 8; i++) {
+      generatedPsd.push(
+        valuesForPsd[Math.floor(Math.random() * (valuesForPsd.length - 1))]
+      );
+    }
+    // console.log(generatedPsd.join(""));
+    const test = generatedPsd.join("");
+    test === ""
+      ? alert("Escolha ao menos um ddos grupos de caracteres")
+      : setCurrentPsd(test);
+
+    setUpperCaseCb(false);
+    setLowerCasecb(false);
+    setNumbersCb(false);
+    setSpecialsCb(false);
+  };
+
+  return (
+    <div className="psd-generator">
+      <h1 classname="titles">Gerador de senhas</h1>
+      <h2 classname="titles">Escolha os tipos de caracteres</h2>
+      <div className="options">
+        <label>Maiúsculas {"(A-Z)"}</label>
+        <input
+          type="checkbox"
+          checked={upperCaseCb}
+          onChange={changeCbValueUpper}
+          className="checkbox-inputs"
+        />
+        <label>Minúsculas {"(a-z)"}</label>
+        <input
+          type="checkbox"
+          checked={lowerCaseCb}
+          onChange={changeCbValueLower}
+          className="checkbox-inputs"
+        />
+        <label>Números {"(0-9)"}</label>
+        <input
+          type="checkbox"
+          checked={numbersCb}
+          onChange={changeCbValueNums}
+          className="checkbox-inputs"
+        />
+        <label>Especiais {"(!-*)"}</label>
+        <input
+          type="checkbox"
+          checked={specialsCb}
+          onChange={changeCbValueSpec}
+          className="checkbox-inputs"
+        />
+      </div>
+      <p>
+        <button
+          onClick={() =>
+            generatePsd(upperCaseCb, lowerCaseCb, numbersCb, specialsCb)
+          }
+          className="generate-psd-btn"
+        >
+          Gerar senha aleatória
+        </button>
+      </p>
+      <h2 classname="titles">{currentPsd}</h2>
+    </div>
+  );
+}
+
+export default PsdGenerator;
